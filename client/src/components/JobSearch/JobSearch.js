f383e48fb9cca1adbbcb0c011e2f70575d3d44c4
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-10-08 06:16:17 -0700

    prep for dev merge

--- client/src/components/JobSearch/JobSearch.js
+++ client/src/components/JobSearch/JobSearch.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
 import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
--- client/src/pages/JobListing/JobListing.js
+++ client/src/pages/JobListing/JobListing.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import ScrollableTabsButtonAuto from '../../components/Tabs/Tabs';
-import Jumbotron from "../../components/Jumbotron";
-import { Col, Row, Container } from "../../components/Grid";
-import Grid from '@material-ui/core/Grid';
-import LabelBottomNavigation from '../../components/Footer/Footer';
-import './JobListing.scss';
-=======
 // import ScrollableTabsButtonAuto from '../../components/Tabs/Tabs';
 // import Jumbotron from "../../components/Jumbotron";
 // import Grid from '@material-ui/core/Grid';
 //import LabelBottomNavigation from '../../components/Footer/Footer'
 import './JobListing.scss'
->>>>>>> a50a7e60d0a7fa99c13a0830f407d1f7f6833694
 //import { Table } from "@material-ui/core";
 import JobSearch from '../../components/JobSearch';
 
