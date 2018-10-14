a53d0ca8f441aa364e0d17cb49f12b51ece56002
Philip Digiglio <37520782+PhilipDig@users.noreply.github.com>
2018-10-14 10:35:12 -0700

    Updated calendar iframe implementation

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -21,7 +21,7 @@ class MyCalendar extends Component {
       <div>
         {
           this.state.googleCalendarId ?
-            <iframe title="Job Piper Calendar" src={`https://calendar.google.com/calendar/embed?height=550&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=${this.state.googleCalendarId}&amp;color=%238C500B&amp;ctz=America%2FLos_Angeles`}
+            <iframe title="Job Piper Calendar" src={`https://calendar.google.com/calendar/embed?height=550&wkst=1&bgcolor=%23FFFFFF&src=${this.state.googleCalendarId}&color=%238C500B&ctz=America%2FLos_Angeles`}
               style={{ border: 0 }} width="800" height="600" frameborder="0" scrolling="no"></iframe> : null
         }
       </div>
