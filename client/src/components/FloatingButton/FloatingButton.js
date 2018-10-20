6bb6d0b2e6a84c9e455193fef55b01cad27c6b4d
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-10-20 13:04:01 -0700

    added loader to joblisting page

--- client/src/components/FloatingButton/FloatingButton.js
+++ client/src/components/FloatingButton/FloatingButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import EditIcon from '@material-ui/icons'
 import Icon from '@material-ui/core/Icon';
 
 
