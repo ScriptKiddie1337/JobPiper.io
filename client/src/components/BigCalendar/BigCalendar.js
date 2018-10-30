56a2840d9674d51dc166f6c8900f853a0c11fcf2
Jesse Loftus <jesse.loftus@gmail.com>
2018-10-30 14:35:44 -0700

    building routes for the new calendar

--- client/src/components/BigCalendar/BigCalendar.js
+++ client/src/components/BigCalendar/BigCalendar.js
@@ -3,11 +3,11 @@ import Calendar from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
-import "./App.css"
+// import "./App.css"
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const localizer = Calendar.momentLocalizer(moment) 
+const localizer = Calendar.momentLocalizer(moment)
 
 const DnDCalendar = withDragAndDrop(Calendar)
 
@@ -16,41 +16,45 @@ class BigCalendar extends Component {
         events: [
             {
                 start: new Date(),
-                end: new Date(moment().add(1,'days')),
-                title: 'Test event'
+                end: new Date(moment().add(1, 'days')),
+                title: 'Test event',
+                allDay:true
             }
         ]
     }
 
     onEventResize = ({ event, start, end, allDay }) => {
-        // console.log(event)
         this.setState(state => {
-          state.events[0].start = start;
-          state.events[0].end = end;
-          return { events: state.events };
-        });
-      };
+            state.events[0].start = start;
+            state.events[0].end = end;
+            return { events: state.events };
+        }, () => console.log(start, end));
+    };
 
-      onEventDrop = ({ event, start, end, allDay }) => {
+    onEventDrop = ({ event, start, end, allDay }) => {
         console.log(start);
-      };
-      
-      render() {
+    };
+    onDoubleClickEvent = (event) => console.log(event)
+
+    render() {
         return (
-          <div>
-            <DnDCalendar
-            localizer={localizer}
-              defaultDate={new Date()}
-              defaultView="month"
-              events={this.state.events}
-              onEventDrop={this.onEventDrop}
-              onEventResize={this.onEventResize}
-              resizable
-              style={{ height: "90vh" }}
-            />
-          </div>
+            <div>
+                <DnDCalendar
+                    localizer={localizer}
+                    selectable="ignoreEvents"
+                    defaultDate={new Date()}
+                    defaultView="month"
+                    events={this.state.events}
+                    onEventDrop={this.onEventDrop}
+                    onEventResize={this.onEventResize}
+                    onDoubleClickEvent={this.onDoubleClickEvent}
+                    resizable
+                    popup="true"
+                    style={{ height: "90vh" }}
+                />
+            </div>
         );
-      }
+    }
 }
 
 export default BigCalendar;
---
+++ routes/api/event.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const eventController = require("../../controllers/eventController");
+
+// event event routes /api/event/
+router
+    .route('/')
+    .get(eventController.getEvents)
+    .put(eventController.updateEvent)
+    .post(eventController.createEvent)
+    .delete(eventController.deleteEvent)
