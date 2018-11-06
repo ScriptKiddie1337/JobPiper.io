e9d9efcebec9d51acb204f7b97804e9ea30b6b8f
Jesse Loftus <jesse.loftus@gmail.com>
2018-11-06 13:11:58 -0800

    events show on big-calendar. changed response calendar logic to account for undefined responses

--- client/src/components/BigCalendar/BigCalendar.js
+++ client/src/components/BigCalendar/BigCalendar.js
@@ -12,7 +12,7 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
+import Checkbox from '@material-ui/core/Checkbox';
 import { auth } from '../../firebase';
 import { initGoogleCalendar, getCalendarEvents, createCalendarEvent, deleteCalendarEvent, updateCalendarEvent } from '../../session/googleCalendar'
 
@@ -36,38 +36,44 @@ class BigCalendar extends Component {
                     .then(res => {
                         if (res.status === 200) {
                             // Update the state with the retrieved calendar events
-                            this.setState({ googleCalEvents: res.result.items })
+                            const eventArray = res.result.items.map(date => ({
+                                start: date.start.dateTime,
+                                end: date.end.dateTime,
+                                title: date.summary,
+                                allDay: false
+                            }))
+                            this.setState({ events: eventArray }, () => console.log(this.state))
 
 
-                            createCalendarEvent(this.state.calendarId, "Job Piper IO", "test event", new Date(moment().subtract(2, 'days')), new Date(moment().add(1, 'days')))
-                                .then(res => {
-                                    if (res.status === 200) {
-                                        // Update state with the event that was sucessfully created
-                                        let calendarEvents = this.state.googleCalEvents
-                                        calendarEvents.push(res.result)
-                                        this.setState({ googleCalEvents: calendarEvents })
+                            // createCalendarEvent(this.state.calendarId, "Job Piper IO", "test event", new Date(moment().subtract(2, 'days')), new Date(moment().add(1, 'days')))
+                            //     .then(res => {
+                            //         if (res.status === 200) {
+                            //             // Update state with the event that was sucessfully created
+                            //             let calendarEvents = this.state.googleCalEvents;
+                            //             (res.result && calendarEvents.push(res.result));
+                            //             this.setState({ googleCalEvents: calendarEvents })
 
-                                        //update the calendar event at index 0
-                                        updateCalendarEvent(this.state.calendarId, this.state.googleCalEvents[0].id, "Job Piper IO2", "updated event!", new Date(moment().add(2, 'days')), new Date(moment().add(4, 'days')))
-                                            .then(res => {
-                                                if (res.status === 200) {
+                            //             //update the calendar event at index 0
+                            //             updateCalendarEvent(this.state.calendarId, this.state.googleCalEvents[0].id, "Job Piper IO2", "updated event!", new Date(moment().add(2, 'days')), new Date(moment().add(4, 'days')))
+                            //                 .then(res => {
+                            //                     if (res.status === 200) {
 
-                                                    let calEvents = this.state.googleCalEvents
-                                                    calEvents[0] = res.result
-                                                    this.setState({ googleCalEvents: calEvents })
-                                                }
-                                            })
+                            //                         let calEvents = this.state.googleCalEvents
+                            //                         calEvents[0] = res.result
+                            //                         this.setState({ googleCalEvents: calEvents })
+                            //                     }
+                            //                 })
 
-                                        // Delete the calendar event at index 0
-                                        // deleteCalendarEvent(this.state.calendarId,this.state.googleCalEvents[0].id)
-                                        // .then(res => {
-                                        //     if(res.status === 204) {
-                                        //         // The first arg of splice is the index of the event you want to remove
-                                        //         this.setState({googleCalEvents: this.state.googleCalEvents.splice(0, 1)})
-                                        //     }
-                                        // })
-                                    }
-                                })
+                            //             //         // Delete the calendar event at index 0
+                            //             //         // deleteCalendarEvent(this.state.calendarId,this.state.googleCalEvents[0].id)
+                            //             //         // .then(res => {
+                            //             //         //     if(res.status === 204) {
+                            //             //         //         // The first arg of splice is the index of the event you want to remove
+                            //             //         //         this.setState({googleCalEvents: this.state.googleCalEvents.splice(0, 1)})
+                            //             //         //     }
+                            //             //         // })
+                            //         }
+                            //     })
                         }
                     })
             })
@@ -119,17 +125,17 @@ class BigCalendar extends Component {
         this.setState({ open: true });
         console.log('All Day: ', this.state.newEvent.allDay);
     };
-    
+
     handleClose = () => {
         this.setState({ open: false });
         console.log('All Day: ', this.state.newEvent.allDay);
     };
-    
+
     handleAllDay = name => event => {
-        let newEvent = {...this.state.newEvent}
+        let newEvent = { ...this.state.newEvent }
         newEvent.allDay = event.target.checked
-        this.setState({newEvent});
-      };
+        this.setState({ newEvent });
+    };
 
     render() {
         const { allDay } = this.state;
@@ -155,17 +161,69 @@ class BigCalendar extends Component {
                 >
                     <DialogTitle id="form-dialog-title">Event Details</DialogTitle>
                     <DialogContent>
+                        {/* Title and all-day flag */}
                         <FormGroup row>
                             <TextField
                                 id="title"
                                 label="Event Title"
                                 margin="normal"
                             />
-                            <Switch
-                                checked={allDay}
-                                onChange={this.handleAllDay('allDay')}
-                                color="primary"
-                                value="allDay"
+                            <FormControlLabel control={
+                                <Checkbox
+                                    checked={allDay}
+                                    onChange={this.handleAllDay('allDay')}
+                                    color="primary"
+                                    value="allDay"
+                                />
+                            }
+                                label="All-Day Event" />
+                        </FormGroup>
+                        {/* Start Date */}
+                        <FormGroup row>
+                            <TextField
+                                id="startDate"
+                                label="Start Date"
+                                type="date"
+                                defaultValue={moment(Date.now()).format("YYYY-MM-DD")}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <TextField
+                                id="startTime"
+                                label="Start Time"
+                                type="time"
+                                defaultValue={moment(Date.now()).format("HH:mm")}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                inputProps={{
+                                    step: 300, // 5 min
+                                }}
+                            />
+                        </FormGroup>
+                        {/* End Date */}
+                        <FormGroup row>
+                            <TextField
+                                id="endDate"
+                                label="End Date"
+                                type="date"
+                                defaultValue={moment(Date.now()).format("YYYY-MM-DD")}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <TextField
+                                id="endTime"
+                                label="End Time"
+                                type="time"
+                                defaultValue={moment(Date.now()).format("HH:mm")}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                inputProps={{
+                                    step: 300, // 5 min
+                                }}
                             />
                         </FormGroup>
 
