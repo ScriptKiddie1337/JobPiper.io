842848dfcaa5818f85bdf6c647896e5a7ae2147a
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-11-05 18:57:32 -0800

    updated login icons

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import { signInPopUp } from "../../firebase/auth";
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import Icon from '@mdi/react';
 import { mdiGoogle } from '@mdi/js';
 import Header from '../../components/Jumbotron'
+import red from '@material-ui/core/colors/red';
+import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/blue';
+import yellow from '@material-ui/core/colors/blue';
+import SvgIcon from '@material-ui/core/SvgIcon';
 
+const styles = theme => ({
+	root: {
+	  display: 'flex',
+	  justifyContent: 'center',
+	  alignItems: 'flex-end',
+	},
+	icon: {
+	  margin: theme.spacing.unit * 2,
+	},
+	iconHover: {
+	  margin: theme.spacing.unit * 2,
+	  '&:hover': {
+		color: red[800],
+	  },
+	},
+  });
+
+  function GIcon(props) {
+	return (
+	  <SvgIcon {...props}>
+		<path d={mdiGoogle} />
+	  </SvgIcon>
+	);
+  }
 class Login extends Component {
 
   signIn = () => {
@@ -22,11 +52,23 @@ class Login extends Component {
         </Grid>
           <Grid item xs={12} style={{ marginTop: '-15vh', display: 'flex', justifyContent: 'center', alignItems: 'flex-start' }}>
             <Button onClick={this.signIn} style={{ backgroundColor: 'white', minWidth: '25vw' }}>
-              <Icon 
+              <GIcon 
                 style={{ margin: '0px 10px 0px 0px' }}
-                path={mdiGoogle} 
                 size={1} 
-                color='red'/>
+                color='red'
+				component={svgProps => (
+					<svg {...svgProps}>
+					  <defs>
+						<linearGradient id="gradient1">
+						  <stop offset="25%" stopColor={red[400]} />
+						  <stop offset="25%" stopColor={yellow[400]} />
+						  <stop offset="25%" stopColor={green[400]} />
+						  <stop offset="25%" stopColor={blue[400]} />
+						</linearGradient>
+					  </defs>
+					  {React.cloneElement(svgProps.children[0], { fill: 'url(#gradient1)' })}
+					</svg>
+				  )}/>
                 Sign In / Create Account
                 </Button>
           </Grid>
@@ -36,4 +78,8 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+	classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(styles)(Login);
