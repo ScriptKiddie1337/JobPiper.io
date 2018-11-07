6f88c0d22da07ec7a427eef2bdf03cb749c8e557
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-11-07 11:50:24 -0800

    updated routes for spreadsheet and created front end components for spreadsheet

---
+++ client/src/components/SpreadSheetList/SpreadSheetList.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { withStyles } from '@material-ui/core/styles';
+import SavedIcon from '@material-ui/icons/StarRate'
+import SaveIcon from '@material-ui/icons/Stars'
+import { auth } from '../../firebase'
+import api from '../../utils/API'
+//import PropTypes from 'prop-types';
+
+const styles = theme => ({
+	root: {
+		flexShrink: 0,
+		color: theme.palette.text.secondary,
+		
+	},
+	heading: {
+		fontSize: theme.typography.pxToRem(15),
+		fontWeight: theme.typography.fontWeightRegular,
+	}
+});
+
+class SpreadSheetList extends Component {
+
+	handleSheetSave = () => {
+
+		this.setState({ saved: true })
+		api.userSaveSheet({
+			...this.props,
+			saved: true,
+			notes: '',
+			dateSaved: Date.now()
+		}, auth.getUserId())
+	}
+//change to delet row
+	// handleJobUnsave = () => {
+	// 	console.log('Unsaving job: ', this.props.title)
+	// 	this.setState({ saved: false })
+	// 	api.userUnsaveJob(this.props._id, auth.getUserId())
+	// }
+
+	state = { saved: this.props.saved }
+
+	componentWillReceiveProps(newProps) {
+
+		this.props = newProps
+
+		if (newProps.saved !== this.state.saved) {
+
+			this.setState({ saved: newProps.saved })
+		}
+	}
+
+	render() {
+
+		const { _id, image, title, link, keywords, body } = this.props
+		const keywordsString = keywords.join(' | ')
+		function createMarkup(val) {
+			return { __html: val };
+		}
+		return (
+			<Paper style={{ backgroundColor: '#FAFAFA', margin: '10px', border: 'solid 3px #819ca9', borderRadius: '5px' }}>
+				<div key={_id} style={{ margin: '10px' }}>
+					<Grid container>
+						<Grid item xs={12} style={{ border: '#fdd835 solid 1px', backgroundColor: '#819ca9', padding: '10px', borderRadius: '5px' }}>
+							<Grid container
+								direction="row"
+								justify="space-between"
+								alignItems="center">
+								<Grid item xs={1}
+									style={{textAlign: 'center'}}>
+									{
+										this.state.saved
+										? <SavedIcon onClick={this.handleJobUnsave} color="secondary" />
+										: <SaveIcon onClick={this.handleJobSave} color='primary' />
+									}
+								</Grid>
+								<Grid item xs={11}>
+									<a href={link} target="_blank" style={{ textDecoration: 'none' }}>
+									<Grid container
+										direction="row"
+										justify="space-between"
+										alignItems="center">
+										<Grid item xs={10} style={{ textAlign: "left" }}><h3 style={{ color: 'white' }} dangerouslySetInnerHTML={createMarkup(title)} />
+										</Grid>
+										<Grid item xs={2} ><img align="right" style={{ maxHeight: "50px" }} src={image} alt={title} />
+										</Grid>
+									</Grid>
+									</a>
+								</Grid>
+							</Grid>
+						</Grid>
+
+						<ExpansionPanel style={{margin: '10px 10px 0px 10px'}}>
+							<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} >
+								<Typography dangerouslySetInnerHTML={createMarkup(keywordsString)} />
+							</ExpansionPanelSummary>
+							<ExpansionPanelDetails>
+								<Grid item xs={12} style={{ padding: '10px 10px 10px 0px' }}
+									dangerouslySetInnerHTML={createMarkup(body)} />
+							</ExpansionPanelDetails>
+						</ExpansionPanel>
+					</Grid>
+				</div>
+			</Paper>
+		)
+	}
+}
+
+export default withStyles(styles)(SpreadSheetList)
